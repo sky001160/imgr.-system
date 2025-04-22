ddf693e4f6cf3938a2b739781468fbc21d11b0d9
sky001160 <tat515hk@gmail.com>
2025-04-23 03:28:46 +0800

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,9 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
+console.log("✅ Firebase 環境變數測試：");
+console.log("API_KEY =", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+console.log("AUTH_DOMAIN =", process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
